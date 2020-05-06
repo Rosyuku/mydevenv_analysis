95ed2871d672e9717479e8b1f8f254fce8230618
Rosyuku <sogenakotoga@homail.com>
2020-05-07 01:47:36 +0900

    jupyter以外はnginxで動作確認

--- volume/python/sample_application/server.py
+++ volume/python/sample_application/server.py
@@ -1,4 +1,6 @@
 from flask_blog import app
+#from waitress import serve
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
+    #serve(app, host='0.0.0.0', port=8080)
